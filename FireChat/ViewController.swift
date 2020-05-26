ec86e1e2f875d0880d2b9ccc6cd077d1b67562dd
Robert Morrow <rcmorrow01@verizon.net>
2020-05-26 13:14:14 -0400

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Robert Morrow on 5/26/20.
+//  Copyright © 2020 Athos, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
