cfcf595725f5657f080730a090fa3a19a7e98147
Robert Morrow <rcmorrow01@verizon.net>
2020-05-28 09:41:29 -0400

    Completed 3, 15 - Added logic to keep user logged in and logic for logging a user in successfully

--- FireChat/Controller/Authentication/LoginController.swift
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 protocol AuthenticationControllerProtocol {
     func checkFormStatus()
@@ -78,7 +79,17 @@ class LoginController: UIViewController {
     // MARK: - Selectors
     
     @objc func handleLogin() {
-        print("Handle Login")
+        guard let email = emailTextField.text else {return}
+        guard let password = passwordTextField.text else {return}
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                if let error = error {
+                    print("DEBUG: Failed to log user in with error: \(error.localizedDescription)")
+                return
+             }
+            
+            self.dismiss(animated: true, completion: nil)
+        }
     }
     
     @objc func handleShowSignUp() {
--- FireChat/Controller/ConversationsController.swift
+++ FireChat/Controller/ConversationsController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 private let reuseIdentifier = "ConversationCell"
 
@@ -22,16 +23,45 @@ class ConversationsController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+        authenticateUser()
         
     }
     
     // MARK: - Selectors
     
     @objc func showProfile() {
-        print("123")
+        logout()
+    }
+    
+    // MARK: - API
+    
+    func authenticateUser() {
+        if Auth.auth().currentUser?.uid == nil {
+            presentLoginScreen()
+        } else {
+            print("DEBUG: User id is: \(Auth.auth().currentUser?.uid)")
+        }
+    }
+    
+    func logout() {
+        do {
+            try Auth.auth().signOut()
+            presentLoginScreen()
+        } catch {
+            print("DEBUG: Error signing user out...")
+        }
     }
     
     // MARK: - Helper Functions
+    
+    func presentLoginScreen() {
+        DispatchQueue.main.async {
+            let controller = LoginController()
+            let nav = UINavigationController(rootViewController: controller)
+            nav.modalPresentationStyle = .fullScreen
+            self.present(nav, animated: true, completion: nil)
+        }
+    }
     
     func configureUI() {
         view.backgroundColor = .white
