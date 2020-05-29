912f13ddb212c97ae22e477d3e679993f0e43c7b
Robert Morrow <rcmorrow01@verizon.net>
2020-05-29 08:54:06 -0400

    Completed 4, 23 Added logic to populate new message users

--- FireChat/Controller/NewMessageController.swift
+++ FireChat/Controller/NewMessageController.swift
@@ -61,7 +61,7 @@ extension NewMessageController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        
+        cell.user = users[indexPath.row]
         return cell
     }
 }
